e80986a4246fd8fa70071ae48675598673d3e49d
Roman Hallouche <halloucheroman@gmail.com>
2019-06-20 18:52:08 -0700

    Adds bash script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/experience.html templates/bottom.html > docs/experience.html
+cat templates/top.html content/education.html templates/bottom.html > docs/education.html
